64ae56eae90c03ecd6656bd992054be74aa07cb3
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-05-31 15:51:31 +0200

    Clean up, untrack folders

--- fstar-missing-files/copy.sh
+++
@@ -1,6 +0,0 @@
-prefix="/home/lucas/FStar/"
-
-cd "$prefix/src/basic/boot/"
-for i in FStar.*.fsi; do
-    cat "$i" | grep -v "// JUST FSHARP" | sed 's/^\/\/ IN F\*: //' > "$prefix/fstar-missing-files/$i"
-done
