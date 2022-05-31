3c0850b9e5ab1fc66096e2d8ab7ec044f10fef93
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-05-31 15:56:11 +0200

    Simplify

--- flake.nix
+++ flake.nix
@@ -8,82 +8,38 @@
   };
   
   outputs = { self, nixpkgs, flake-utils, fstar-flake }:
-    let
-      dir_locals = ''
-((fstar-mode
-  (fstar-subp-sloppy . nil)
-  (eval .
-	(progn (defun my-fstar-compute-prover-args-using-make ()
-		 "Construct arguments to pass to F* by calling make."
-		 (with-demoted-errors "Error when constructing arg string: %S"
-		   (split-string
-		    (car (last (process-lines "make"
-					      "-C" (append (getenv "FSTAR_SOURCES_ROOT") "/src")
-					      "-f" "Makefile.edit" "--quiet"
-					      (concat (file-name-nondirectory buffer-file-name) "-dev")
-					      )
-			       )
-			 )
-		    )
-		   )
-		 )
-	       (setq fstar-subp-prover-args #'my-fstar-compute-prover-args-using-make)
-	       )
-	)
-  )
- )
-'';
-      makefile_edit = ''
-FSTAR_HOME = ..
-include Makefile.boot.common
-ABS_CACHE_DIR = $(abspath $(CACHE_DIR))
-%.fsi-dev: $(ABS_CACHE_DIR)/%.fsi.checked.lax
-	@echo $(FSTAR_BOOT_OPTIONS)
-%.fs-dev: $(ABS_CACHE_DIR)/%.fs.checked.lax
-	@echo $(FSTAR_BOOT_OPTIONS)'';
-    in
-      flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux"]
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            fstar = fstar-flake.defaultPackage.${system};
-            lib = pkgs.lib;
-          in  
-            rec {
-              packages = {
-                fstar = fstar.override ({ name = "fstar-dev";
-                                          src = pkgs.lib.cleanSource ./.;
-                                        });
-              };
-              defaultPackage = packages.fstar;
-              devShell = pkgs.mkShell {
-                name = "dev-env-fstar-compiler";
-                buildInputs = fstar.buildInputs ++ [
-                  fstar
-                  (pkgs.writeScriptBin "cp-fstar"
-                    ''cp  --no-preserve=all ${fstar}/bin/fstar.exe ./bin/fstar.exe
-                      chmod +x ./bin/fstar.exe'')
-                  (pkgs.writeScriptBin "generate-ocaml" ''make ocaml -C src -j6'')
-                  (pkgs.writeScriptBin "build-ocaml"
-                    ''{rm ./bin/fstar.exe.old || true} &> /dev/null
-                      mv ./bin/fstar.exe ./bin/fstar.exe.old
-                      if make -C src/ocaml-output ../../bin/fstar.exe; then
-                        rm ./bin/fstar.exe.old;
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux"]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          fstar = fstar-flake.defaultPackage.${system};
+          lib = pkgs.lib;
+        in  
+          rec {
+            packages = {
+              fstar = fstar.override ({ name = "fstar-dev";
+                                        src = pkgs.lib.cleanSource ./.;
+                                      });
+            };
+            defaultPackage = packages.fstar;
+            devShell = pkgs.mkShell {
+              name = "dev-env-fstar-compiler";
+              inputsFrom = [ packages.fstar ];
+              buildInputs = [
+                (pkgs.writeScriptBin "fstar.exe"
+                  ''#!${pkgs.bash}/bin/bash
+                      if test -f "$FSTAR_SOURCES_ROOT"/bin/fstar.exe; then
+                         "$FSTAR_SOURCES_ROOT"/bin/fstar.exe "$@"
                       else
-                        mv ./bin/fstar.exe.old ./bin/fstar.exe
+                        echo "WARNING: NOT USING LOCAL FSTAR.EXE"
+                        ${fstar}/bin/fstar.exe "$@"
                       fi
-                  '')
-                ];
-                shellHook = ''
-                  bash fstar-missing-files/copy.sh || true
-                  rm ./bin/fstar.exe || true
-                  cp  --no-preserve=all ${fstar}/bin/fstar.exe ./bin/fstar.exe
-                  chmod +x ./bin/fstar.exe
+                    '')
+              ];
+              shellHook = ''
                   export FSTAR_SOURCES_ROOT="$(pwd)"
-                  echo ${lib.escapeShellArg makefile_edit} > src/Makefile.edit
-                  echo ${lib.escapeShellArg dir_locals} > .dir-locals.el
                 '';
-              };
-            }
-        );
+            };
+          }
+      );
 }
