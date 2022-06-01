d9a90cc3733aa3d0456b8d5772caddc7dd744a0b
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-06-01 21:38:40 +0200

    Write `src/.dir-locals.el` automatically

--- flake.nix
+++ flake.nix
@@ -38,7 +38,29 @@
               ];
               shellHook = ''
                   export FSTAR_SOURCES_ROOT="$(pwd)"
+                  echo "$DIR_LOCALS" > src/.dir-locals.el
                 '';
+              DIR_LOCALS = ''((fstar-mode
+  (fstar-subp-sloppy . nil)
+  (eval .
+  (progn (defun my-fstar-compute-prover-args-using-make ()
+             "Construct arguments to pass to F* by calling make."
+             (with-demoted-errors "Error when constructing arg string: %S"
+               (split-string
+                (car (last (process-lines "make"
+                             "-C" (concat (getenv "FSTAR_SOURCES_ROOT") "/src")
+                             "-f" "Makefile.boot" "--quiet"
+                             (concat (file-name-nondirectory buffer-file-name) "-in")
+                           )
+                     )
+                )
+               )
+             )
+         )
+         (setq-local fstar-subp-prover-args #'my-fstar-compute-prover-args-using-make)
+  )
+)))
+'';
             };
           }
       );
